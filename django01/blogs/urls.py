6f51a77542239aaf948709474391a037d0f060c7
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-03-04 16:32:17 +0500

    update function updated

--- django01/blogs/urls.py
+++ django01/blogs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import blogListView, blogDetailView, blogCreateView, blogUpdateVIew
 
 urlpatterns = [
-    path('post/edit', blogUpdateVIew.as_view(), 'post_update'),
+    path('post/edit/<int:pk>', blogUpdateVIew.as_view(), name='post_update'),
     path('post/new', blogCreateView.as_view(), name='post_create'),
     path('', blogListView.as_view(), name='home'),
     path('post/<int:pk>', blogDetailView.as_view(), name='post_detail' )

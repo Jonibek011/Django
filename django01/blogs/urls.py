153f2b3e8b5f51c9526cfcf9a64d6e4ba03a794a
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-03-10 09:35:44 +0500

    updated

--- django01/blogs/urls.py
+++ django01/blogs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import blogListView, blogDetailView, blogCreateView, blogUpdateVIew
+from .views import blogListView, blogDetailView, blogCreateView, blogUpdateVIew, blogDeleteView
 
 urlpatterns = [
+    path('post/delete/<int:pk>', blogDeleteView.as_view(), name='post_delete'),
     path('post/edit/<int:pk>', blogUpdateVIew.as_view(), name='post_update'),
     path('post/new', blogCreateView.as_view(), name='post_create'),
     path('', blogListView.as_view(), name='home'),
--- django01/blogs/views.py
+++ django01/blogs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -25,3 +26,8 @@ class blogUpdateVIew(UpdateView):
     model = Post
     template_name = 'post_upodate.html'
     fields = ['title', 'body']
+
+class blogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')

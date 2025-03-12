132d48bc48afc3223a5eb43b0248a30705983c96
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-03-12 12:25:48 +0500

    procfile added

--- django02/config/settings.py
+++ django02/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-svxc@emt(8o)zz0b07va%fq$#^d88pvqdxbmxyny@(k4*r^(v-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

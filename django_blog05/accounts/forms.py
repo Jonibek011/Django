a298fda787322e75ddd41ec38400e78759597d62
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-03-14 09:00:01 +0500

    17-dars

---
+++ django_blog05/accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomModel
+
+
+
+class CustomUserCreation(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomModel
+        fields = ['username', 'first_name', 'last_name', 'email', 'age']
+
+class CustomUserChange(UserChangeForm):
+    class Meta:
+        model = CustomModel
+        fields = ['username', 'first_name', 'last_name', 'email', 'age']
+
+        

1bc4bbdd927b25dff371e45186b1ef51db46fedf
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-03-31 16:24:33 +0500

    tailwind added

--- django_blog06/accounts/views.py
+++ django_blog06/accounts/views.py
@@ -5,5 +5,5 @@ from .forms import CustomUserCreation
 # Create your views here.
 class SignUpView(CreateView):
     form_class = CustomUserCreation
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'

0169bf499ad4e43681af236fa1550c9a960c3b88
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-04 08:33:19 +0500

    21-dars

--- django_blog01/articles/views.py
+++ django_blog01/articles/views.py
@@ -11,7 +11,7 @@ class ArticleView(ListView):
     template_name = 'article_list.html'
 
 
-class ArticleDetail(DetailView):
+class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_detail.html'
 

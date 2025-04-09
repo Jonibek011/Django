f7c3b3b001140ad677c30d739a6dc6405791d2b1
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-09 19:38:06 +0500

    21-dars

--- django_blog01/articles/views.py
+++ django_blog01/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView,  DetailView
 from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from .models import Article
@@ -15,18 +16,33 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_detail.html'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Article
-    fields = ['title', 'summary', 'body', 'photo', 'author']
+    fields = ['title', 'summary', 'body', 'photo', ]
     template_name = 'article_new.html'
 
-class ArticleUpdateView(UpdateView):
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'summary', 'body', 'photo')
     template_name = 'article_update.html'
 
+    def test_func(self):
+        obj = self.get_object
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object
+        return obj.author == self.request.user
